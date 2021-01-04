59d87799dbd080a165670a571a0ab716d3307714
Sy Rashid <sy.rashid.11@gmail.com>
2021-01-03 22:06:47 -0500

    ARCH: Base rails app generation using BambooSticks

---
+++ README.md
@@ -0,0 +1,5 @@
+# >>PLEASE UPDATE ME<<
+
+Rails app generated with BambooSticks, based on the LeWagon Devise template & Thoughtbot Suspenders.
+
+Developed & Designed by MangoTree Dev Agency
---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  get '/kitchensink', to: 'pages#kitchensink' if Rails.env.development?
+  root to: 'pages#home'
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
