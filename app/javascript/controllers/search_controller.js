a60f7166ea85d3d761152ee1ed9448f97747377b
Sy Rashid <sy.rashid.11@gmail.com>
2021-01-04 12:17:42 -0500

    FEAT: Threw js code into a stimulus controller

---
+++ app/javascript/controllers/search_controller.js
@@ -0,0 +1,52 @@
+// Visit The Stimulus Handbook for more details
+// https://stimulusjs.org/handbook/introduction
+//
+// This example controller works with specially annotated HTML like:
+//
+// <div data-controller="hello">
+//   <h1 data-target="hello.output"></h1>
+// </div>
+
+import { Controller } from "stimulus"
+
+import algoliasearch from "algoliasearch";
+import autocomplete from "autocomplete.js";
+
+export default class extends Controller {
+  static targets = [ ]
+
+  connect() {
+    console.log("Sweet Tits");
+    var client = algoliasearch('BA4HJTMZZL', 'e48d96f1f9ae255ccfaec1e8704eebad');
+    var index = client.initIndex('Pokemon');
+
+    function newHitsSource(index, params) {
+      return function doSearch(query, cb) {
+        index
+        .search(query, params)
+        .then(function(res) {
+          cb(res.hits, res);
+        })
+        .catch(function(err) {
+          console.error(err);
+          cb([]);
+        });
+      };
+    }
+
+    autocomplete('#search-input', { hint: false }, [
+      {
+        source: newHitsSource(index, { hitsPerPage: 10 }),
+        displayKey: 'name',
+        templates: {
+          suggestion: function(suggestion) {
+            // Change the return here to whatever you wish to be displayed in the dropdown
+            return `${suggestion._highlightResult.name.value} lives in ${suggestion._highlightResult.location.value} and uses ${suggestion._highlightResult.move.value}`;
+          }
+        }
+      }
+    ]).on('autocomplete:selected', function(event, suggestion, dataset, context) {
+      console.log(event, suggestion, dataset, context);
+    });
+  }
+}
--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -16,7 +16,7 @@ require("channels")
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-import "controllers"
+
 
 
 // ----------------------------------------------------
@@ -32,40 +32,12 @@ import autocomplete from "autocomplete.js";
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 
+import "controllers"
+
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
-var client = algoliasearch('BA4HJTMZZL', 'e48d96f1f9ae255ccfaec1e8704eebad');
-var index = client.initIndex('Pokemon');
 
-function newHitsSource(index, params) {
-    return function doSearch(query, cb) {
-      index
-        .search(query, params)
-        .then(function(res) {
-          cb(res.hits, res);
-        })
-        .catch(function(err) {
-          console.error(err);
-          cb([]);
-        });
-    };
-  }
-
-  autocomplete('#search-input', { hint: false }, [
-    {
-      source: newHitsSource(index, { hitsPerPage: 10 }),
-      displayKey: 'name',
-      templates: {
-        suggestion: function(suggestion) {
-            // Change the return here to whatever you wish to be displayed in the dropdown
-            return `${suggestion._highlightResult.name.value} lives in ${suggestion._highlightResult.location.value} and uses ${suggestion._highlightResult.move.value}`;
-        }
-      }
-    }
-  ]).on('autocomplete:selected', function(event, suggestion, dataset, context) {
-    console.log(event, suggestion, dataset, context);
-  });
 });
 
 
